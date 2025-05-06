7326e14ef5af5bf24ee3ca7fda9881f03b763814
KohKumasan <12067_takeuchi_ko@schools.kobe-u.ac.jp>
2025-05-06 11:28:33 +0900

    content　guestHPに整理券情報追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-Togetan is cutest creature in the world!!!!
+<h1>整理券システムについてのご案内</h1>
+<p>本企画では整理券システム <a href="https://matene.jp/">MATENE</a> を使用しております</p>
+お手持ちの整理券番号を以下のサイトに入力してください
+<p>整理券番号を入力することで、あなたの整理券番号が何番目かを確認できます</p>
+<p>当日のイベント用URLを入れる</p>
 
 
 <script lang="ts">
