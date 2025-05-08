075e8bac28f9870ee4d4126a18b96236ac639b72
Shoji Tamaki <shoji.tamaki@gmail.com>
2025-05-09 08:32:41 +0900

    APIエンドポイントのベースを追加

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,8 +2,34 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+// API Documentation is in ../README.md
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('This is the backend API for jurabbit')
+})
+
+// enable betting (for operators)
+app.post('/enable_betting', (c) => {
+
+})
+
+// disable betting (for operators)
+app.post('/disable_betting', (c) => {
+})
+
+// get info about horses (for users)
+app.get('/horses', (c) => {
+})
+
+// input the results of a race (for operators)
+app.post('/results', (c) => {
+})
+
+// get winners by race id (for users)
+app.get('/winners/:race_id', (c) => {
+})
+
+// issue a bet (for users)
+app.post('/bet', (c) => {
 })
 
 export default app
