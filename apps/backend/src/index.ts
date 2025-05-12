f1343d6bc954154443564040b0d1cda113ab42a2
Shoji Tamaki <shoji.tamaki@gmail.com>
2025-05-12 15:17:46 +0900

    DBに接続できたぞー！（結果投稿/取得APIを含む）

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,8 +1,13 @@
 import { Hono } from 'hono'
-import { KVNamespace } from '@cloudflare/workers-types'
+import { D1Database, KVNamespace } from '@cloudflare/workers-types'
+import { drizzle } from 'drizzle-orm/d1'
+import { eq } from 'drizzle-orm'
+import { z } from 'zod'
+import { results } from './schema'
 
 type Bindings = {
-  jurabbit_mode: KVNamespace
+  jurabbit_mode: KVNamespace,
+  jurabbit_store: D1Database
 }
 
 const app = new Hono<{Bindings: Bindings}>()
@@ -45,13 +50,72 @@ app.get('/betting_status', async (c) => {
   });
 })
 
-// // get info about horses (for users)
+// get info about horses (for users)
+// リクエスト数の削減のため、このAPIはフロントエンドのSSG時に呼び出され、一般のユーザーが直接利用することはない
 // app.get('/horses', (c) => {
+  
 // })
 
-// // input the results of a race (for operators)
-// app.post('/results', (c) => {
-// })
+// input the results of a race (for operators)
+app.post('/results', async (c) => {
+  try {
+    if (!c.env.jurabbit_store) {
+      return c.json({ error: 'D1 storage is not available' }, 500);
+    }
+
+    const body = await c.req.json();
+
+    const ResultSchema = z.object({
+      raceId: z.number().int().positive(),
+      results: z.array(z.object({
+        horse_id: z.number().int().positive(),
+        rank: z.number().int().positive().positive()
+      })).nonempty()
+    });
+
+    const parsedResult = ResultSchema.safeParse(body);
+
+    if (!parsedResult.success) {
+      return c.json(
+        {
+          error: 'Invalid request body',
+          details: parsedResult.error.format()
+        }, 400);
+    }
+
+    const parsedBody = parsedResult.data;
+
+    const db = drizzle(c.env.jurabbit_store);
+
+    const resultData = parsedBody.results.map(result => ({
+      raceId: parsedBody.raceId,
+      horseId: result.horse_id,
+      rank: result.rank
+    }));
+
+    await db.insert(results).values(resultData);
+    
+    return c.json({
+      message: 'Results inserted successfully',
+    })
+  } catch (error) {
+    console.error('Error inserting results:', error);
+    return c.json({ error: 'Internal server error' }, 500);
+  }
+})
+
+// get results
+app.get('/results/:id', async (c) => {
+  if (!c.env.jurabbit_store) {
+    return c.json({ error: 'D1 database is not available' }, 500);
+  }
+
+  const requiredRaceId = parseInt(c.req.param('id'), 10);
+  const db = drizzle(c.env.jurabbit_store);
+
+  const searchedResults = await db.select().from(results).where(eq(results.raceId, requiredRaceId));
+  return c.json(searchedResults);
+})
 
 // // get winners by race id (for users)
 // app.get('/winners/:race_id', (c) => {
