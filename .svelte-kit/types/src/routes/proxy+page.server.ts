3ba203adb061caaac925abd5bb57b54e6339b572
KohKumasan <12067_takeuchi_ko@schools.kobe-u.ac.jp>
2025-05-16 05:02:40 +0900

    style 背景画像の設定

---
+++ .svelte-kit/types/src/routes/proxy+page.server.ts
@@ -0,0 +1,15 @@
+// @ts-nocheck
+import type { PageServerLoad } from "./$types";
+
+import fs from 'fs/promises';
+import path from 'path';
+
+
+export const load = async () => {
+    const filePath = path.resolve('src/lib/matene_url.json');
+    const data = JSON.parse(await fs.readFile(filePath, 'utf-8'));
+    return {
+    eventUrl: data.url
+    };
+}
+;null as any as PageServerLoad;
--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<main class="min-h-screen bg-slate-50 py-10 px-4 sm:px-6 lg:px-8">
+<main class="min-h-screen bg-slate-50 py-10 px-4 sm:px-6 lg:px-8" style="background-image: url('/image/background.jpg')">
   <div class="max-w-3xl mx-auto">
     <h1 class="text-3xl font-bold text-center text-blue-800 mb-8">第13回兎原祭　Dinoshow@運動場（晴天の場合）</h1>
     
