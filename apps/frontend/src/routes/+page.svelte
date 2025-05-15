91bcfcb64aec3178cb12fe20de7a4f5fc423ab15
KohKumasan <12067_takeuchi_ko@schools.kobe-u.ac.jp>
2025-05-16 05:47:51 +0900

    style トップ画面追加

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <main class="min-h-screen bg-slate-50 py-10 px-4 sm:px-6 lg:px-8" style="background-image: url('/image/background.jpg')">
+  <img src="apps/frontend/static/image/top.png" alt="トップ画面">
   <div class="max-w-3xl mx-auto">
     <h1 class="text-3xl font-bold text-center text-blue-800 mb-8">第13回兎原祭　Dinoshow@運動場（晴天の場合）</h1>
     
